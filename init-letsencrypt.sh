b8e53a7c999c900d6af7c79410b897927de5ca93
Michal J. Gasior <michaljgasior@gmail.com>
2020-09-25 23:46:04 +0200

    Set one domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(cracker.red www.cracker.red)
+domains="cracker.red"
 rsa_key_size=4096
 data_path="./data/certbot"
 email="michaljgasior@gmail.com" # Adding a valid address is strongly recommended
