9c67ccfb29e64a09f776b09b979439828d58a835
Michal J. Gasior <michaljgasior@gmail.com>
2020-09-25 23:48:38 +0200

    Define one domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -52,10 +52,12 @@ echo
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
 #Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+#domain_args=""
+#for domain in "${domains[@]}"; do
+#  domain_args="$domain_args -d $domain"
+#done
+
+domain_args=" -d $domains"
 
 # Select appropriate email arg
 case "$email" in
